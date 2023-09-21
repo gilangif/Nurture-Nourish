705e4c2bd6786af0d7d2ad8129b35702f3112550
gilangif <gilangidulfitri1201@gmail.com>
2023-09-21 20:08:15 +0700

    remove: junk file

--- routes/user_route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const UserController = require('../controllers/userController');
-
-router.post('/users', UserController.createUser)
-
-module.exports = router;
