233c3bf205b4714f49967aef67bde9b781bbe99d
Rama Edwinda Putra <ramaedwindap@gmail.com>
2023-09-25 10:34:29 +0700

    feat: create auth screen for welcome, login, register, and create profile

--- Nurture-Nourish/src/navigator/MainStack.js
+++ Nurture-Nourish/src/navigator/MainStack.js
@@ -1,14 +1,58 @@
-import { createStackNavigator } from "@react-navigation/stack"
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Chat from "../screens/Chat"
 import Dashboard from "../screens/Dashboard"
 import Camera from "../screens/Camera"
 import Preview from "../screens/Preview"
 import Test from "../screens/Test"
-
-const Stack = createStackNavigator()
+import WelcomeScreen from '../screens/WelcomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import CreateProfileScreen from '../screens/CreateProfile';
+import { useState } from 'react';
 
 export default function MainStack() {
+  const Stack = createNativeStackNavigator();
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const loggedIn = true;
+      setIsAuthenticated(loggedIn);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (!isAuthenticated) {
+    return (
+      <LayoutScreen>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Welcome"
+            component={WelcomeScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Register"
+            component={RegisterScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="CreateProfile"
+            component={CreateProfileScreen}
+          />
+        </Stack.Navigator>
+      </LayoutScreen>
+    );
+  }
+
   return (
     <Stack.Navigator>
       <Stack.Screen name="Dashboard" component={Dashboard} />
@@ -19,3 +63,16 @@ export default function MainStack() {
     </Stack.Navigator>
   )
 }
+
+
+// export default function MainStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Dashboard" component={Dashboard} />
+//       <Stack.Screen name="Chat" component={Chat} options={{ headerShown: false }} />
+//       <Stack.Screen name="Camera" component={Camera} options={{ headerShown: false }} />
+//       <Stack.Screen name="Preview" component={Preview} />
+//       <Stack.Screen name="Test" component={Test} />
+//     </Stack.Navigator>
+//   )
+// }
